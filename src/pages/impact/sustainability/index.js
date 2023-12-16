bd4a70e5f3c9c90896177f283e4be3f1227b0399
cptkuk91 <cptkuk91@gmail.com>
2023-12-17 00:54:54 +0900

    [수정] global.scss 및 reverse 추가

--- src/pages/impact/sustainability/index.js
+++ src/pages/impact/sustainability/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './index.module.scss';
-// import teaserConent from '../../../assets/images/teaserContent.webp';
 import TeaserContent from './TeaserContent';
 import Sustainable from './Sustainable';
 import Double from './Double';
+
+// Double Image
+import campaign1 from '../../../assets/images/campaign1.webp';
+import campaign2 from '../../../assets/images/campaign2.webp';
 
 const cx = classNames.bind(styles);
 
@@ -24,7 +27,20 @@ const Page = () => {
       </div>
       <TeaserContent />
       <Sustainable />
-      <Double />
+      <h2 className={cx('doubleTitle')}>지속 가능성 글로벌 리포트</h2>
+      <Double
+        reverse={false}
+        contentTitle="Business & Sustainability Report"
+        contentDescription="코카-콜라는 지속 가능한 미래를 가꾸고 널리 사랑받는 브랜드와 제품을 만듭니다. 사람들의 갈증을 해소하고 더
+            나아가 긍정적인 변화를 이루기 위한 노력을 확인해보세요."
+        image={campaign1}
+      />
+      <Double
+        reverse={true}
+        contentTitle="SUSTAINABILITY RESOURCE CENTER"
+        contentDescription="코카-콜라는 지속 가능한 미래를 만들기 위해 노력합니다. 기업의 운영에서부터 사람들의 일상까지 더 나은 미래를 만들기 위한 코카-콜라의 노력을 확인해보세요."
+        image={campaign2}
+      />
     </div>
   );
 };
