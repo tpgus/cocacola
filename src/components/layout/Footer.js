a10545b93cf2c87fafa4c86214ecaeaaf256e6c9
Sehyun <sehyun.kim@themobilityone.com>
2023-12-17 12:05:59 +0900

    brand 메뉴 구성 및 footer 구성

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import { MdOutlineLocationOn } from 'react-icons/md';
+import { RiFacebookLine, RiTwitterXLine, RiInstagramLine, RiYoutubeFill } from 'react-icons/ri';
 
 import styles from './Footer.module.scss';
+import { MENUS } from '../../constants/footer';
+import { ReactComponent as Logo } from '../../assets/images/logo-white-large.svg';
 
 const cx = classNames.bind(styles);
 
 const Footer = () => {
-  return <div className={cx('container')}>푸터 영역</div>;
+  return (
+    <div className={cx('container')}>
+      <div className={cx('top')}>
+        <Logo />
+        <a href="https://www.coca-cola.com/country-selector">
+          <MdOutlineLocationOn size={24} />
+          <span>한국</span>
+        </a>
+      </div>
+      <div className={cx('middle')}>
+        <div className={cx('left')}>
+          {MENUS.map((menu) => (
+            <div className={cx('navs')}>
+              <div className={cx('title')}>{menu.title}</div>
+              <ul className={cx('menus')}>
+                {menu.subMenus.map((submenu) => (
+                  <li className={cx('menu')}>{submenu.title}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className={cx('right')}>
+          <div className={cx('links')}>
+            <RiYoutubeFill className={cx('icon')} />
+            <RiTwitterXLine className={cx('icon')} />
+            <RiInstagramLine className={cx('icon')} />
+            <RiFacebookLine className={cx('icon')} />
+          </div>
+        </div>
+      </div>
+      <div className={cx('bottom')}>© 2023 The Coca‑Cola Company. 판권 소유</div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ src/constants/footer.js
@@ -0,0 +1,25 @@
+export const MENUS = [
+  {
+    title: '소개',
+    subMenus: [
+      { title: '기업 소개', path: 'https://www.coca-cola.com/kr/ko/about-us/our-company' },
+      { title: '히스토리', path: 'https://www.coca-cola.com/kr/ko/about-us/history' },
+      { title: '채용', path: 'https://www.coca-colacompany.com/careers' },
+    ],
+  },
+  {
+    title: '도움이 필요하세요?',
+    subMenus: [
+      { title: '사이트 맵', path: 'https://www.coca-cola.com/kr/ko/sitemap' },
+      { title: '고객 안내', path: 'https://www.coca-cola.com/kr/ko/about-us/contact-us' },
+    ],
+  },
+  {
+    title: '법적고지',
+    subMenus: [
+      { title: '이용 약관', path: 'https://www.coca-cola.com/kr/ko/legal/terms-of-service' },
+      { title: '쿠키 정책', path: 'https://www.coca-cola.com/kr/ko/legal/cookie-policy' },
+      { title: '쿠키 정책', path: 'https://www.coca-cola.com/kr/ko/legal/cookie-policy' },
+    ],
+  },
+];
